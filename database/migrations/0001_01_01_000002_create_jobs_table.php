86d491f9cb31416e02238f956c78ee11f304424c
johndavis1999 <jdyglol123@gmail.com>
2024-11-20 02:33:58 -0500

    fix migrations sessions 2

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('queue')->index();
+            $table->string('queue', 191)->index();
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
             $table->unsignedInteger('reserved_at')->nullable();
@@ -22,7 +22,7 @@ return new class extends Migration
         });
 
         Schema::create('job_batches', function (Blueprint $table) {
-            $table->string('id')->primary();
+            $table->string('uuid', 191)->unique(); 
             $table->string('name');
             $table->integer('total_jobs');
             $table->integer('pending_jobs');
